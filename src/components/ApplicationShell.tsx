8d0140b7ef56f4f7b8f62c2b4439bddf6bcbb997
Mohammed Rifaque C <Rifaue.c@applab.qa>
2026-06-02 17:47:08 +0530

    fix: correct import statement in ApplicationShell component

--- src/components/ApplicationShell.tsx
+++ src/components/ApplicationShell.tsx
@@ -1,4 +1,4 @@
-vidimport { useEffect, type PropsWithChildren, type ReactNode } from 'react'
+import { useEffect, type PropsWithChildren, type ReactNode } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Link, useLocation } from 'react-router-dom'
 
